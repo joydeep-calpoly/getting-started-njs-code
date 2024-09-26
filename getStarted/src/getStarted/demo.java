41a51f0937a49fd30ca0d755e718e266773a2b62
Noah Snodgrass <419noah@protonmail.com>
2024-09-26 15:17:49 -0700

    Added some print messages

--- getStarted/src/getStarted/demo.java
+++ getStarted/src/getStarted/demo.java
@@ -4,5 +4,7 @@ public class demo {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to CSC 305");
+		System.out.println("Thanks!");
+		System.out.println("Does this make it unstaged?");
 	}
 }
