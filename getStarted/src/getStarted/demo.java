d823f527c6872f9a2ca28a8a8f1e84f4caf35c3a
Noah Snodgrass <419noah@protonmail.com>
2024-09-26 17:57:43 -0700

    Removed a print message

--- getStarted/src/getStarted/demo.java
+++ getStarted/src/getStarted/demo.java
@@ -5,6 +5,5 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("Welcome to CSC 305");
 		System.out.println("Thanks!");
-		System.out.println("Does this make it unstaged?");
 	}
 }
