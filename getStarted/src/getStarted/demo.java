5d7def284d4a02cb31e16eed43782b693192cd96
Noah Snodgrass <419noah@protonmail.com>
2024-09-26 17:58:22 -0700

    test again

--- getStarted/src/getStarted/demo.java
+++ getStarted/src/getStarted/demo.java
@@ -4,6 +4,6 @@ public class demo {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to CSC 305");
-		System.out.println("Thanks!");
+		System.out.println("Print message tests");
 	}
 }
